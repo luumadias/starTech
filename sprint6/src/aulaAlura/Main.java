fb094d5d97974f9ee01d4c37a4b0bdedf3f566f5
Luma <luumadiaslima@gmail.com>
2024-02-01 14:23:43 -0300

    .

---
+++ sprint6/src/aulaAlura/Main.java
@@ -0,0 +1,16 @@
+package aulaAlura;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Barbie");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+
+        String sinopse;
+        sinopse = "Filme sobre sobre a vivência feminina";
+        System.out.println(sinopse);
+    }
+}
+
